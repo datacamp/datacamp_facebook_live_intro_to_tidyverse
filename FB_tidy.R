85f5ea59c78d48153f19a873c78a508fd580a91f
Hugo Bowne-Anderson <hugo@datacamp.com>
2018-03-22 17:04:12 +1100

    add data + first draft of solution code

---
+++ FB_tidy.R
@@ -0,0 +1,101 @@
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+# Load data ---------------------------
+
+# Import data
+titanic <- read.csv("data/train.csv")
+
+# Check out the head of your dataframe
+head(titanic)
+
+# Check out head of titanic using a pipe!
+titanic %>% head()
+
+# Summarize titanic
+titanic %>% summary()
+
+# Summarize titanic after dropping na
+titanic %>% drop_na() %>% summary()
+
+# Wrangling your data ---------------------------
+
+# Subset to get all 'Male' rows
+titanic %>% filter(Sex == "male") %>% head()
+
+# Subset to get all 'Female' rows
+titanic %>% filter(Sex == "female") %>% head()
+
+# Arrange by increasing Fare
+titanic %>% arrange(Fare) %>% head()
+
+# # Arrange by decreasing Fare
+titanic %>% arrange(desc(Fare)) %>% head()
+
+# Create new column fam_size (size of family)
+titanic %>% mutate(fam_size = Parch + SibSp)
+
+# Create new column fam_size (size of family)
+# Arrange by decreasing FamSize
+titanic %>% mutate(fam_size = Parch + SibSp) %>%
+  arrange(desc(fam_size)) %>%  head()
+
+# Turn numerical values of Survived column to 'No' & 'Yes' (new dataframe)
+titanic1 <- titanic %>%
+  mutate(Survived = replace(Survived, Survived == 0, "No")) %>%
+  mutate(Survived = replace(Survived, Survived == 1, "Yes"))
+
+# Plot data ---------------------------
+
+# Plot barplot of passenger Sex
+ggplot(titanic1, aes(Sex)) + geom_bar()
+
+# Plot barplot of passenger Sex & stack according to Survival
+ggplot(titanic1, aes(Sex)) + geom_bar(aes(fill = Survived))
+
+# Scatter plot of Age vs Fare
+ggplot(titanic, aes(x = Age, y = Fare)) + geom_point()
+
+# Scatter plot of Age vs Fare colored by Sex
+ggplot(titanic, aes(x = Age, y = Fare, color = Sex)) + geom_point()
+
+# Scatter plot of Age vs Fare colored by Sex faceted by Survived
+ggplot(titanic1, aes(x = Age, y = Fare, color = Sex)) + geom_point() +
+  facet_wrap(~ Survived)
+
+
+# Summarize data ---------------------------
+
+# Check out mean Fare
+titanic %>% 
+  summarize(meanFare = mean(Fare))
+
+# Check out mean Fare
+titanic %>% 
+  summarize(medianFare = median(Fare))
+
+# Check out mean Fare for men
+titanic %>%
+  filter(Sex == 'male') %>% 
+  summarize(meanFare = mean(Fare))
+
+# Check out mean Fare for women
+titanic %>%
+  filter(Sex == 'female') %>% 
+  summarize(meanFare = mean(Fare))
+
+# Check out mean Fare & number of survivors for women
+titanic %>%
+  filter(Sex == 'female') %>% 
+  summarize(meanFare = mean(Fare), Survived = sum(Survived))
+
+# Check out mean Fare & number of survivors grouped by Sex
+titanic %>% 
+  group_by(Sex) %>% 
+  summarise(meanFare = mean(Fare), Survived = sum(Survived))
+
+# Check out mean Fare & proportion of survivors grouped by Sex
+titanic %>% 
+  group_by(Sex) %>% 
+  summarise(meanFare = mean(Fare), Survived = sum(Survived)/n())
